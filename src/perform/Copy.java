a1d5c6159b17ff818d3c26e70ee6b4746ac39952
indivisible-irl <mbspare@gmail.com>
2012-12-28 17:20:05 +0000

    begin copy

--- src/perform/Copy.java
+++ src/perform/Copy.java
@@ -1,5 +1,15 @@
 package perform;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import items.Job;
 import items.MyFile;
 import items.MyFolder;
@@ -19,7 +29,55 @@ public class Copy {
 	{
 		for (MyFile file : folder.getFiles())
 		{
-			sfsf
+			//TODO
 		}
+	}
+	
+	private static boolean copyFile(MyFile sFile, File destFile )
+	{
+		File sourceFile = sFile.getSelf();
+		
+		if (!sourceFile.canRead())
+			return false;
+		
+		InputStream in = null;
+		OutputStream out = null;
+		
+		try
+		{
+			in = new BufferedInputStream(new FileInputStream(sourceFile), 1024);
+			out = new BufferedOutputStream(new FileOutputStream(destFile), 1024);
+			
+			
+			return true;
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (in != null)
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			if (out != null)
+				try {
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		
+		return false;
+	}
+	
+
+	private static boolean verifyCopySuccessful(MyFile sFile, File destFile)
+	{
+		//TODO compare md5 and (?) size
+		return true;
 	}
 }
